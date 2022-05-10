388c9a3b5e2f86381bcd40a3d64ab56bae440a79
Elton Bautista <eltonbestaha@gmail.com>
2022-05-10 14:28:10 -0600

    Changed handleIncrementScore to function declaration Moved cardArray into App instead of CardContainer

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import Header from './components/Header';
 import Scoreboard from './components/Scoreboard';
 import CardContainer from './components/CardContainer';
 import { useState } from 'react';
+import Card from './components/Card';
 
 function App() {
   const [currentScore, setCurrentScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
+  const cardArray = 
+  [
+  <Card incrementScore={handleIncrementScore} imageAlt='Astra' imageSrc={require('./assets/astra.png')} />,
+  <Card incrementScore={handleIncrementScore} imageAlt='Breach' imageSrc={require('./assets/breach.png')} />,
+  <Card incrementScore={handleIncrementScore} imageAlt='Brimstone' imageSrc={require('./assets/brimstone.png')} />,
+  <Card incrementScore={handleIncrementScore} imageAlt='Chamber' imageSrc={require('./assets/chamber.png')} />,
+  <Card incrementScore={handleIncrementScore} imageAlt='Cypher' imageSrc={require('./assets/cypher.png')} />
+  ];
 
   // logic for incrementing score
-  const handleIncrementScore = () => {
+
+  function handleIncrementScore() {
     setCurrentScore(currentScore + 1);
     setBestScore(bestScore + 1);
   }
@@ -24,6 +34,7 @@ function App() {
       />
       <CardContainer
         incrementScore={handleIncrementScore}
+        myCards={cardArray}
       />
     </div>
   );
