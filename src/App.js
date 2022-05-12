336595a4f660ee0d15b4c170d79b1953d86d414d
Elton Bautista <eltonbestaha@gmail.com>
2022-05-12 06:49:56 -0600

    Changed myAgentProfiles Used my factoryFunctionAgent module to create the appropriate agent profiles A public method in the factory function is used to turn isSelected attribute to true Also added comments

--- src/App.js
+++ src/App.js
@@ -3,52 +3,42 @@ import Header from './components/Header';
 import Scoreboard from './components/Scoreboard';
 import CardContainer from './components/CardContainer';
 import { useEffect, useState } from 'react';
-import Card from './components/Card';
+import factoryFunctionAgent from './modules/factoryFunctionAgent';
 
 function App() {
+  // State for keeping track of my scores
   const [currentScore, setCurrentScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
   
+  // State for my agent profiles utilizing a factory function
   const myAgentProfiles = 
   {
-    astra: {
-      imageAlt: 'Astra',
-      imageSrc: '../assets/astra.png',
-      isSelected: false,
-    },
-    breach: {
-      imageAlt: 'Breach',
-      imageSrc: '../assets/breach.png',
-      isSelected: false,
-    },
-    brimstone: {
-      imageAlt: 'Brimstone',
-      imageSrc: '../assets/brimstone.png',
-      isSelected: false,
-    },
-    chamber: {
-      imageAlt: 'Chamber',
-      imageSrc: '../assets/chamber.png',
-      isSelected: false,
-    },
-    cypher: {
-      imageAlt: 'Cypher',
-      imageSrc: '../assets/cypher.png',
-      isSelected: false,
-    },
+    astra: factoryFunctionAgent('astra', '', false),
+    breach: factoryFunctionAgent('breach', '', false),
+    brimstone: factoryFunctionAgent('brimstone', '', false),
+    chamber: factoryFunctionAgent('chamber', '', false),
+    cypher: factoryFunctionAgent('cypher', '', false),
   }
+  const [profile, setProfile] = useState(myAgentProfiles);
 
-const [profile, setProfile] = useState(myAgentProfiles);
+  // My onClick function that is passed down to my cards
+  // It increments score, as well as changes the card's data-selected attribute to true
+  // Utilizes a public method for security purposes
 
   function handleIncrementScore(e) {
-    const name = e.target.name;
+    // alt is used for object property accessing via square brack notation 
+    
+    const alt = e.target.alt;
 
     setCurrentScore(currentScore + 1);
     setBestScore(bestScore + 1);
 
+    // A deep copy of profile is created
+    // The deep copy changes the appropriate agent's isSelected prop to true
+    // The deep copy is passed into profile as the new state
+
     const myUpdatedProfile = {...profile};
-    myUpdatedProfile[name].isSelected = true;
-    console.log(myUpdatedProfile);
+    myUpdatedProfile[alt].isSelectedTrue();
 
     setProfile(myUpdatedProfile);
 
@@ -63,7 +53,6 @@ const [profile, setProfile] = useState(myAgentProfiles);
         bestScore={bestScore} 
       />
       <CardContainer
-        // myCards={cardArray}
         incrementScore={handleIncrementScore}
         agentProfiles={profile}
       />
