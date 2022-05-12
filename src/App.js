b0dd42c48cab002e4a9f4e46e973165662f6344e
Elton Bautista <eltonbestaha@gmail.com>
2022-05-12 07:20:54 -0600

    Created an effect that checks win condition also wrote conditional to check if user loses The useEffect gets the agent images collection and turns them into an iterable array The conditional in handleIncrementScore checks if the card isSelected is false

--- src/App.js
+++ src/App.js
@@ -21,26 +21,44 @@ function App() {
   }
   const [profile, setProfile] = useState(myAgentProfiles);
 
+  // an effect that checks if all agent isSelected is 'true';
+
+  useEffect(() => {
+    const [...myAgents] = document.querySelectorAll('.agent');
+    const booleanArray = [];
+    const checkIfAllTrue = (bool) => bool === 'true';
+
+    myAgents.forEach((agent) => {
+      booleanArray.push(agent.dataset.selected);
+    })
+    
+    if (booleanArray.every(checkIfAllTrue)) {
+      console.log('game over');
+    }
+  }, [profile])
+
   // My onClick function that is passed down to my cards
   // It increments score, as well as changes the card's data-selected attribute to true
   // Utilizes a public method for security purposes
 
   function handleIncrementScore(e) {
-    // alt is used for object property accessing via square brack notation 
-    
+    // alt is used for object property accessing via square brack notation
     const alt = e.target.alt;
-
-    setCurrentScore(currentScore + 1);
-    setBestScore(bestScore + 1);
+    
+    if (e.target.dataset.selected === 'false') {
+      setCurrentScore(currentScore + 1);
+      setBestScore(bestScore + 1);
 
     // A deep copy of profile is created
     // The deep copy changes the appropriate agent's isSelected prop to true
     // The deep copy is passed into profile as the new state
 
-    const myUpdatedProfile = {...profile};
-    myUpdatedProfile[alt].isSelectedTrue();
-
-    setProfile(myUpdatedProfile);
+      const myUpdatedProfile = {...profile};
+      myUpdatedProfile[alt].isSelectedTrue();
+  
+      setProfile(myUpdatedProfile);
+    }
+    // console.log('lose function here');
 
   }
 
